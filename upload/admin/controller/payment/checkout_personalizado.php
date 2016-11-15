88c0ed981e6e48458c2225cca4685e39fc549c3e
Michael Granados <michaelgranados@gmail.com>
2016-11-15 13:33:31 -0200

    adding some tokens

--- upload/admin/controller/payment/checkout_personalizado.php
+++ upload/admin/controller/payment/checkout_personalizado.php
@@ -7,6 +7,10 @@ class ControllerCheckoutPersonalizado extends Controller {
         $data['heading_title'] = $this->language->get('heading_title');
         $data['breadcrumb'] = array();
 
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+
         $output = $this->load->view('payment/checkout_personalizado.tpl', $data);
         $this->response->setOutput($output);
     }
