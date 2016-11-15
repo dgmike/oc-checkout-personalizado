d44f34f18a0fa8591c72c29389be66cc37361f1f
Michael Granados <michaelgranados@gmail.com>
2016-11-15 14:56:53 -0200

    cleaning index action

--- upload/admin/controller/payment/checkout_personalizado.php
+++ upload/admin/controller/payment/checkout_personalizado.php
@@ -1,17 +1,30 @@
 <?php
 
 class ControllerPaymentCheckoutPersonalizado extends Controller {
+    public $data = array();
+
+    public function __construct($registry) {
+        parent::__construct($registry);
+        $this->defaultData();
+    }
+
     public function index() {
+        $this->view('payment/checkout_personalizado');
+    }
+
+    private function defaultData() {
         $this->load->language('payment/checkout_personalizado');
 
-        $data['heading_title'] = $this->language->get('heading_title');
-        $data['breadcrumbs'] = array();
+        $this->data['heading_title'] = $this->language->get('heading_title');
+        $this->data['breadcrumbs'] = array();
 
-        $data['header'] = $this->load->controller('common/header');
-        $data['column_left'] = $this->load->controller('common/column_left');
-        $data['footer'] = $this->load->controller('common/footer');
+        $this->data['header'] = $this->load->controller('common/header');
+        $this->data['column_left'] = $this->load->controller('common/column_left');
+        $this->data['footer'] = $this->load->controller('common/footer');
+    }
 
-        $output = $this->load->view('payment/checkout_personalizado.tpl', $data);
+    private function view($template) {
+        $output = $this->load->view("${template}.tpl", $this->data);
         $this->response->setOutput($output);
     }
 }
