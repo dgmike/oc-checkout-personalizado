1801235cfb06a339e2822dd9e080ca75bb7fbb59
Michael Granados <michaelgranados@gmail.com>
2016-11-15 14:04:05 -0200

    typo

--- upload/admin/controller/payment/checkout_personalizado.php
+++ upload/admin/controller/payment/checkout_personalizado.php
@@ -5,7 +5,7 @@ class ControllerPaymentCheckoutPersonalizado extends Controller {
         $this->load->language('payment/checkout_personalizado');
 
         $data['heading_title'] = $this->language->get('heading_title');
-        $data['breadcrumb'] = array();
+        $data['breadcrumbs'] = array();
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
